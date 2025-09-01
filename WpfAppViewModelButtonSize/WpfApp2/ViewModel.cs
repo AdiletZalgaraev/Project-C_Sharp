7270362872dedb4690a2352e0a52402ee2c6e119
Adilet <127068760+AdiletZalgaraev@users.noreply.github.com>
2025-09-01 21:44:21 +0500

    Add files via upload

---
+++ WpfAppViewModelButtonSize/WpfApp2/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApp2
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string Name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+
+        int h, w;
+
+        public int H
+        {
+            get { return h;}
+            set 
+            {
+                h = value;
+                Notify("H");
+            }
+        }
+
+        public int W
+        {
+            get { return w; }
+            set
+            {
+                w = value;
+                Notify("W");
+            }
+        }
+    }
+}
---
+++ WpfAppViewModelCalculator/WpfApp1/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        double a, b, sum;
+
+        public double A 
+        { 
+            get { return a; }
+            set { a = value;
+                Sum = a + b;
+                Notify("A");
+            }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                b = value;
+                Sum = a + b;
+                Notify("B");
+            }
+        }
+
+        public double Sum
+        {
+            get { return sum; }
+            set
+            {
+                sum = value;
+                Notify("Sum");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify (string name)
+        {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (name));
+        }
+    }
+}
